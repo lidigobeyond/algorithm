dc8832c6b74b2b10af7db1997c2a84747dd77ad3
lidigobeyond <khkh4631@gmail.com>
2025-04-20 23:56:15 +0900

    [Baekjoon] BOJ_11052, BOJ_16194

---
+++ baekjoon/src/boj_11052/Main.java
@@ -0,0 +1,25 @@
+package boj_11052;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] p = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            p[i] = sc.nextInt();
+        }
+
+        int[] d = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                d[i] = Math.max(d[i], d[i-j] + p[j]);
+            }
+        }
+
+        System.out.println(d[n]);
+    }
+}
